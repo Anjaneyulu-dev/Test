2a7d20720d8a913c3c3c8f8ce2371d7022eb2802
Anjaneyulu-dev <67837375+Anjaneyulu-dev@users.noreply.github.com>
2020-10-16 13:54:06 +0530

    Apple Sample
    Sample

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPApple.Models;
+using Microsoft.AspNetCore.Authentication;
+
+namespace ASPApple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TestChallenge()
+        {
+            var result = await HttpContext.AuthenticateAsync();
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return Challenge("apple");
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync("cookie");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ASPApple
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddAuthentication(options =>
+           {
+                options.DefaultAuthenticateScheme = "cookie";
+                options.DefaultChallengeScheme = "apple";
+            })
+                .AddCookie("cookie")
+                .AddOpenIdConnect("apple", async options =>
+                {
+                    options.ResponseType = "code";
+                    options.SignInScheme = "cookie";
+                    options.DisableTelemetry = true;
+                    options.Scope.Clear(); // otherwise I had consent request issues
+
+                    options.Configuration = new OpenIdConnectConfiguration
+                    {
+                        AuthorizationEndpoint = "https://appleid.apple.com/auth/authorize",
+                        TokenEndpoint = "https://appleid.apple.com/auth/token",
+                    };
+
+                    //options.ClientId = "com.scottbrady91.authdemo.service"; // Service ID
+                    options.ClientId = "com.apoyar.sai.service"; // Service ID                    
+                    options.CallbackPath = "/signin-apple"; // corresponding to our redirect URI
+
+                    options.Events.OnAuthorizationCodeReceived = context =>
+                    {
+                        context.TokenEndpointRequest.ClientSecret = TokenGenerator.CreateNewToken();
+                        return Task.CompletedTask;
+                    };
+
+                    // Expected identity token iss value
+                    options.TokenValidationParameters.ValidIssuer = "https://appleid.apple.com";
+
+                    // Expected identity token signing key
+                    var jwks = await new HttpClient().GetStringAsync("https://appleid.apple.com/auth/keys");
+                    options.TokenValidationParameters.IssuerSigningKey = new JsonWebKeySet(jwks).Keys.FirstOrDefault();
+
+                    // Disable nonce validation (not supported by Apple)
+                    options.ProtocolValidator.RequireNonce = false;
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+    public static class TokenGenerator
+    {
+        public static string CreateNewToken()
+        {
+            const string iss = "62QM29578N"; // your accounts team ID found in the dev portal
+            const string aud = "https://appleid.apple.com";
+            const string sub = "com.scottbrady91.authdemo.service"; // same as client_id
+
+            const string privateKey = "MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgnbfHJQO9feC7yKOenScNctvHUP+Hp3AdOKnjUC3Ee9GgCgYIKoZIzj0DAQehRANCAATMgckuqQ1MhKALhLT/CA9lZrLA+VqTW/iIJ9GKimtC2GP02hCc5Vac8WuN6YjynF3JPWKTYjg2zqex5Sdn9Wj+";
+            var cngKey = CngKey.Import(Convert.FromBase64String(privateKey), CngKeyBlobFormat.Pkcs8PrivateBlob);
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.CreateJwtSecurityToken(
+                issuer: iss,
+                audience: aud,
+                subject: new ClaimsIdentity(new List<Claim> { new Claim("sub", sub) }),
+                expires: DateTime.UtcNow.AddMinutes(5), // expiry can be a maximum of 6 months => generate one per request, or one and then re-use until expiration
+                issuedAt: DateTime.UtcNow,
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(new ECDsaSecurityKey(new ECDsaCng(cngKey)), SecurityAlgorithms.EcdsaSha256));
+
+            return handler.WriteToken(token);
+        }
+    }
+}
